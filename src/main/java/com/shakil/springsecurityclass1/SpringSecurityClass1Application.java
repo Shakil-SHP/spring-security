d0e6a6a5817348cde87ab1639120b23f8b2086f9
Md. Shakil Hossain <shakilhossainpavel@gmail.com>
2022-05-01 00:46:45 +0600

    Basic application with defualt auth

---
+++ src/main/java/com/shakil/springsecurityclass1/SpringSecurityClass1Application.java
@@ -0,0 +1,13 @@
+package com.shakil.springsecurityclass1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityClass1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityClass1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/shakil/springsecurityclass1/SpringSecurityClass1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shakil.springsecurityclass1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityClass1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
